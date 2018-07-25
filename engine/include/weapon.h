59cdacaae1a9564c196d51135c36c521e3b09cbb
Andrewjvb <finerockguitar@gmail.com>
2018-07-24 22:01:04 -0700

    Create weapon interface, disable tests build until content is added.

---
+++ engine/include/weapon.h
@@ -0,0 +1,10 @@
+#include "weapon_interface.h"
+
+namespace simhammer
+{
+    class weapon : public weapon_interface
+    {
+        
+    };
+
+}
---
+++ engine/include/weapon_interface.h
@@ -0,0 +1,31 @@
+#include <memory>
+#include <vector>
+
+// TODO av include modifier class
+class modifier_interface;
+
+namespace simhammer
+{
+
+    class weapon_interface
+    {
+    public:
+        using modifier_ptr = std::unique_ptr<modifier_interface>;
+        using modifier_vector = std::vector<modifier_ptr>;
+        using weapon_stats = std::vector<int>;
+        /*
+         * \brief return modified weapon obj
+         * \param mod a single modifier to apply
+         * \param mods a vector of modifiers to apply  
+         */
+        virtual void apply_modifiers(modifier_ptr mod) = 0;
+        virtual void apply_modifiers(modifier_vector mods) = 0;
+
+        virtual ~weapon_interface() = default;
+
+        virtual const weapon_stats& get_weapon_stats_vector() const = 0;
+    
+    };
+
+    using weapon_interface_ptr = std::unique_ptr<weapon_interface>;
+}
