316c09430da757d579b2d7c0bb73c2a68bef05f6
Andrewjvb <finerockguitar@gmail.com>
2018-07-24 22:18:20 -0700

    Add member overrides for weapon.h

--- engine/include/weapon.h
+++ engine/include/weapon.h
@@ -4,7 +4,17 @@ namespace simhammer
 {
     class weapon : public weapon_interface
     {
+    public:
+        explicit weapon(const weapon_stats& stats) = default;
+
+        void apply_modifiers(modifier_ptr mod) override;
+        void apply_modifiers(modifier_vector mods) override;
         
+        const weapon_stats& get_weapon_stats_vector() const override;
+    
+    private:
+
+        weapon_stats weapon_stats_;
     };
 
 }
