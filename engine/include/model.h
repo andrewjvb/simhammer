2c4684d5ec850d6d4eb02c123e6f35cc33460b7d
Vakidis <nick_bakich@yahoo.com>
2018-07-27 02:04:51 -0700

    cleanup

--- engine/include/model.h
+++ engine/include/model.h
@@ -2,22 +2,9 @@
 #include "weapon.h"
 
 namespace simhammer {
-    typedef struct {
-      int movement;
-      int weapon_skill;
-      int ballistic_skill;
-      int strength;
-      int toughness;
-      int wounds;
-      int attacks;
-      int leadership;
-      int armor_save;
-      int points;
-    } datasheet_t;
-
     class Model {
     public:
-        explicit Model(datasheet_t *datasheet);
+	explicit Model(const stats_map& stats) : statline_(stats) {}
         ~Model() = default;
 
 	/* Model functions */
@@ -35,5 +22,34 @@ namespace simhammer {
     private:
       stats_map statline_;
       std::vector<Weapon> wep_;
-};
+    };
+
+    /* Builder */
+    std::unique_ptr<Model> make_model(
+        int movement,
+        int weapon_skill,
+        int ballistic_skill,
+        int strength,
+        int toughness,
+        int wounds,
+        int attacks,
+        int leadership,
+        int armor_save,
+        int points) {
+	    return std::make_unique<Model>(
+	        []() {
+                    stats_map stats;
+	            stats[stat_key::MOVEMENT       ] = movement;
+	            stats[stat_key::WEAPON_SKILL   ] = weapon_skill;
+	            stats[stat_key::BALLISTIC_SKILL] = ballistic_skill;
+	            stats[stat_key::STRENGTH       ] = strength;
+	            stats[stat_key::TOUGHNESS      ] = toughness;
+	            stats[stat_key::WOUNDS         ] = wounds;
+	            stats[stat_key::ATTACKS        ] = attacks;
+	            stats[stat_key::LEADERSHIP     ] = leadership;
+	            stats[stat_key::ARMOR_SAVE     ] = armor_save;
+	            stats[stat_key::POINTS         ] = points;
+	            return stats;
+	        });
+        }
 }
--- engine/src/model.cc
+++ engine/src/model.cc
@@ -1,19 +1,1 @@
 #include "model.h"
-
-namespace simhammer
-{
-
-/* Create model with datasheet */
-Model::Model(datasheet_t *datasheet) {
-    statline_[MOVEMENT       ] = datasheet->movement;
-    statline_[WEAPON_SKILL   ] = datasheet->weapon_skill;
-    statline_[BALLISTIC_SKILL] = datasheet->ballistic_skill;
-    statline_[STRENGTH       ] = datasheet->strength;
-    statline_[TOUGHNESS      ] = datasheet->toughness;
-    statline_[WOUNDS         ] = datasheet->wounds;
-    statline_[ATTACKS        ] = datasheet->attacks;
-    statline_[LEADERSHIP     ] = datasheet->leadership;
-    statline_[ARMOR_SAVE     ] = datasheet->armor_save;
-    statline_[POINTS         ] = datasheet->points;
-}
-}
